3cc7cf7a66b96bc42eef585f99ad11f1a7863c5c
f0rest <wladislawfedoseev@gmail.com>
2021-10-22 19:33:35 +0300

    addSubmitInSignInPage

--- src/pages/SignInPage/index.jsx
+++ src/pages/SignInPage/index.jsx
@@ -1,20 +1,38 @@
 import React from "react";
+import "./SingInPage.css"
 import { Link } from "react-router-dom";
+import LoginForm from "../../components/SingInPage"
+import logo  from "../../assets/image/logo.png";
 
 export default function SignInPage() {
 
 
+    const onSubmit = (values, formikBag) => {
+        console.log(values);
+
+        formikBag.resetForm();
+    };
+
+    console.log(onSubmit)
+
     return (
-        <section>
-            <header>
+        <section className="signIn">
+            <header className="headerSignIn">
                 {/* <Link to="/"> */}
-                <img src="/images/logo.png" alt="LOGO" />
+                <img src={logo} alt="LOGO" />
                 {/* </Link> */}
-                <Link to="/SignUpPage">
-                    <button>SingUp</button>
+                <Link to="/signup">
+                    <div className="btnSing">
+                        <a href="/singUpPage">Singup</a>
+                    </div>
                 </Link>
             </header>
-            <h1>LOGIN TO YOUR ACCOUNT</h1>
+            <div className="Login">
+                <h1>LOGIN TO YOUR ACCOUNT</h1>
+            </div>
+            <div className="LoginForm">
+                <LoginForm onSubmit={onSubmit} />
+            </div>
         </section>
     );
 }
