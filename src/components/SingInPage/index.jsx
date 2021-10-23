354706735d145076baa1a6eda0efaac3911f7447
f0rest <wladislawfedoseev@gmail.com>
2021-10-23 16:26:07 +0300

    addSCEMEonPage

--- src/components/SingInPage/index.jsx
+++ src/components/SingInPage/index.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Form, Formik, ErrorMessage } from "formik";
-import Input from "../Input"
-import "./SingInPage.css"
+import Input from "../Input";
+import { SIGNUP_SCHEME } from "../../validation/validationShemesIN.js"
+import "./SingInPage.scss";
+
 // import googlelogo from "../../assets/image/google-brands.svg"
 
 const initialValues = {
@@ -10,30 +12,27 @@ const initialValues = {
 
 };
 
-
 export default function LoginForm(props) {
+    
     return (
         <Formik
             initialValues={initialValues}
+            validationSchema={SIGNUP_SCHEME}
             onSubmit={props.onSubmit}
         >
             {(formikProps) => {
                 return (
                     <Form className="form">
-
                         <legend className="legendInput">
-                            <div className="inputRelative">
-                                <label for="name" className="labelName">Email adress</label>
-                                <Input id="name" type="text" name="email" className="inputStyle" placeholder="Email adress"{...formikProps} />
-                            </div>
-                            <label for="password" className="labelName"></label>
+                            <Input type="text" name="email"   className="inputStyle" placeholder="Email adress"{...formikProps} />
+                            <ErrorMessage name="email" className="errors"{...formikProps} />
                             <Input id="password" type="text" name="password" className="inputStyle" placeholder="Password"{...formikProps} />
-                            <ErrorMessage name="confPassword" {...formikProps} />
+                            <ErrorMessage name="password" className="errors" {...formikProps} />
                         </legend>
                         <legend>
                             <div className="frgtpassword">
                                 <div className="remeberMe">
-                                    <input id="frgt" type="checkbox" name="remeberMe" />
+                                    <input id="frgt" type="checkbox" name="remeberMe" onChange={{}} />
                                     <p>Remeber Me</p>
                                 </div>
                                 <div><a href=" ">Forgot password</a></div>
@@ -41,7 +40,7 @@ export default function LoginForm(props) {
                         </legend>
                         <legend className="legendBtn">
                             <button type="submit" className="btnStyle  btnLogin">LOGIN</button>
-                            <button type="submit" className="btnStyle btngoogle"><img src={{}} alt="logo" />Sing in with Google</button>
+                            <button type="button" className="btnStyle btngoogle">Sing in with Google</button>
                         </legend>
                     </Form>
                 );
