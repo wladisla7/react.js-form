4e5c55b4736080bf157a8164a27012b94ce33d6a
f0rest <wladislawfedoseev@gmail.com>
2021-11-01 18:08:50 +0200

    SingUpSubmit

--- src/pages/SignUpPage/index.jsx
+++ src/pages/SignUpPage/index.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import RegisrationForm from "../../components/SingUpPage"
+import logo from "../../assets/image/logo.png";
  
 
+
+
 export default function SignUpPage() {
+
+  const onSubmit = (values, formikBag) => {
+    console.log(values);
+    formikBag.resetForm();
+  };
+
+  console.log(onSubmit)
+
   return (
-    <section>
-      <Link to="/" style={{ backgroundColor: "#555", display: "inline-block" }}>
-        <img src="/images/logo.png" alt="LOGO" style={{ display: "block" }} />
-      </Link>
-      <h1>CREATE AN ACCOUNT</h1>
-      <h2>We always keep your name and email address private.</h2>
+    <section className="sign">
+      <header className="headerSign">
+        <img src={logo} alt="LOGO" />
+        <Link to="/">
+          <div className="btnSing">
+            <span>Login</span>
+          </div>
+        </Link>
+      </header>
+      <div className="Login">
+        <h1>CREATE AN ACCOUNT</h1>
+        <p className="private">We always keep your name and email address private.</p>
+      </div>
+      <div className="Form">
+        <RegisrationForm onSubmit={onSubmit} />
+      </div>
     </section>
   );
 }
