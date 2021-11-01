a5289c3974b18218155e2da5338dce46b6908155
f0rest <wladislawfedoseev@gmail.com>
2021-11-01 18:10:53 +0200

    addClassNameLogic

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { ErrorMessage, Field } from "formik";
+import cn from "classnames";
+import styles from "./Input.module.scss"
 
+export default function Input({ name, errors, touched, values,  className, ...rest }) {
 
-export default function Input ({ name, errors, touched, values, ...rest }) {
+    const classNames = cn("input",className,{
+        [styles.valid]: touched[name] && !errors[name],
+        [styles.invalid]: touched[name] && errors[name],
+    });
+
+    console.log(errors)
 
     return (
-        false,
         (
-            <label>
-                <Field name={name} {...rest} />
-                <ErrorMessage name={name} />
+            <label className={styles.relative}>
+                <Field    name={name} {...rest}   className={classNames}  />
+                <ErrorMessage name={name} className={styles.errorMsg}  component="span"  />
             </label>
         )
     );
